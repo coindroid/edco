b2443da3a1b70bc7a1ed8346f679c761d445c427
edu-coin <admin@edu-coin.online>
2019-02-25 17:21:06 +0000

    1

--- src/version.h
+++ src/version.h
@@ -12,30 +12,30 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80860;
+static const int PROTOCOL_VERSION = 92260;
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 80860;
+static const int INIT_PROTO_VERSION = 92260;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 80860;
+static const int GETHEADERS_VERSION = 92260;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 80859;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 80860;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 92260;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 80860;
+static const int CADDR_TIME_VERSION = 92260;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 80860;
+static const int BIP0031_VERSION = 92260;
 
 //! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 80860;
+static const int MEMPOOL_GD_VERSION = 92260;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 80860;
+static const int NO_BLOOM_VERSION = 92260;
 
 
 #endif // BITCOIN_VERSION_H
